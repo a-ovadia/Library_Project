ad1b4b4d5dfe15d7275733a043b58b838fcd455b
Adam Ovadia <adam@aovadia.com>
2024-06-10 09:34:10 -0400

    Enforce Library card value is numeric

--- Class Src/Person.py
+++ Class Src/Person.py
@@ -4,9 +4,11 @@ class Person:
 
     def __init__(self, name, library_card_num = 0, genre = "", books = None):
         self._name = name
-        self._library_card_number = library_card_num
         self._favorite_genre = genre
         self._loaned_books = [books]
+        if not library_card_num.isnumeric():
+            raise ValueError("Library card must be numeric")
+        self._library_card_number = library_card_num
 
     
     # Define setters
@@ -14,6 +16,8 @@ class Person:
         self._name = name
 
     def add_library_card(self, library_card):
+        if not library_card.isnumeric():
+            raise ValueError("Library card must be numeric")
         self._library_card_number = library_card
     
     def set_favorite_genre(self, genre):
