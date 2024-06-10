c2e984b249b3264f9175c773b4c0816a26861de2
Adam Ovadia <adam@aovadia.com>
2024-06-10 09:36:33 -0400

    Start LibraryManager class (WIP)

---
+++ Class Src/LibraryManager.py
@@ -0,0 +1,17 @@
+from Library import Library
+from Books import Book
+from Person import Person
+from BookCollection import BookCollection
+
+
+# From the perspective of a Librarian
+# Interface between Library and user input 
+class LibraryManager:
+
+    def __init__(self) -> None:
+        pass
+
+    def main_menu(self):
+
+     
+
